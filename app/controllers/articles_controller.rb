198fd0fcf01eed31d817267befc25689ef333a06
diathanine <perryachan@gmail.com>
2020-10-29 01:05:41 +0800

    forgot to add updating, added it.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,6 +24,15 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def update
+    @article = Article.find params[:id]
+    if @article.update article_params
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
   def destroy
     @article = Article.find params[:id]
     @article.destroy
