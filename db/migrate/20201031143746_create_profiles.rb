166c94bf0396242fdae36b458d731c0bc3d5081d
diathanine <perryachan@gmail.com>
2020-10-31 23:07:37 +0800

    created profiles
    a seperate model to associate with users and articles, this is what gets displayed to the viewer, also will hold bio etc

---
+++ db/migrate/20201031143746_create_profiles.rb
@@ -0,0 +1,11 @@
+class CreateProfiles < ActiveRecord::Migration[6.0]
+  def change
+    create_table :profiles do |t|
+      t.text :bio
+      t.references :article, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_30_164241) do
+ActiveRecord::Schema.define(version: 2020_10_31_143746) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -28,6 +28,16 @@ ActiveRecord::Schema.define(version: 2020_10_30_164241) do
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
+  create_table "profiles", force: :cascade do |t|
+    t.text "bio"
+    t.integer "article_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["article_id"], name: "index_profiles_on_article_id"
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -42,4 +52,6 @@ ActiveRecord::Schema.define(version: 2020_10_30_164241) do
   end
 
   add_foreign_key "comments", "articles"
+  add_foreign_key "profiles", "articles"
+  add_foreign_key "profiles", "users"
 end
