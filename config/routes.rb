df8697eedd724067ae496093b56869db6a586c8d
diathanine <diathanine@gmail.com>
2020-11-01 09:09:34 +0800

    nested profiles resource

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
 
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :profiles
-  resources :articles do
-    resources :comments
+  resources :profiles do
+    resources :articles do
+      resources :comments
+    end
   end
   root 'articles#index'
 end
